4a273d6bb0273b1f44b5a1e46a07d93853b494a2
Garrison LeRock <garrison.lerock@kochcc.com>
2024-05-09 16:35:16 -0500

    added saml and oauth checks

--- http_endpoint_check.py
+++ http_endpoint_check.py
@@ -5,6 +5,16 @@ import json
 import math
 
 import www_authenticate
+import argparse
+import urllib.parse
+import re
+
+
+parser=argparse.ArgumentParser(description="HTTP Endpoint Check")
+parser.add_argument('-r', action='store_true', help="return raw output from urls")
+parser.add_argument("urls", help="list of urls to check")
+
+raw_output = False
 
 
 class bcolors:
@@ -40,27 +50,63 @@ headers = {
 #     "Accept": "*/*",
 # }
 
-auth_map = {
-    "APIGW":{
-        "Source":"Amazon AWS",
-        "Name":"API Gateway",
-        "Description":"You need an API key, likely in header as x-api-key. See more info at https://aws.amazon.com/what-is/api-key/",
-    }
-}
 
 
-def check_auth_type(response):
+
+def check_auth_on_300(response):
 
     #document various scenarios, start from the known auth attributes, look for headers and codes
     #then parse everything else
-    print(response)
+    #print(response)
+
+    headers = response.headers
+    location = urllib.parse.unquote(headers["Location"])
+    #print(location)
+
+    #saml keywords for HTTP binding using SAML 2.0
+    # according to https://sagarag.medium.com/reloading-saml-saml-basics-b8999995c73e
+    saml_kws = ["sso","saml","samlrequest","relaystate","samlencoding","sigalg"]
+    if any(ext in location.lower() for ext in saml_kws):
+        #This is saml redirect HTTP binding
+        # try to extract this info
+        # Regular expressions to extract SAMLRequest, RelayState, and IDP
+        saml_request_pattern = r"SAMLRequest=([^&]+)"
+        relay_state_pattern = r"RelayState=([^&]+)"
+        idp_pattern = r"=([^&]+)?SAMLRequest"
+
+        # Extracting values using regular expressions
+        saml_request =  urllib.parse.unquote(re.search(saml_request_pattern, location).group(1))
+        relay_state =  urllib.parse.unquote(re.search(relay_state_pattern, location).group(1))
+        idp =  urllib.parse.unquote(re.search(idp_pattern, location).group(1))
+
+        if raw_output:
+            print("SAMLRequest:", saml_request)
+            print("RelayState:", relay_state)
+            print("IDP:", idp)
+
+        return f"SAML Auth","SAML SSO",f"HTTP SAML Binding found. Identity Provider is {idp}"
+
+
+
+    #print(response.headers)
+    # look for oidc
+    if "Set-Cookie" in headers:
+        if "oidc_id_token" in headers["Set-Cookie"]:
+            return "OIDC Auth","OAUTH","OAuth found."
+
+
+def check_auth_type_on_400(response):
+
+    #document various scenarios, start from the known auth attributes, look for headers and codes
+    #then parse everything else
+    #print(response)
 
     headers = response.headers
 
     #print(response.headers)
 
-    if "x-amz-apigw-id" in headers and r.status_code == 403:
-        return auth_map["APIGW"]
+    if "x-amz-apigw-id" in headers and response.status_code == 403:
+        return "Amazon AWS","API Gateway","You need an API key, likely in header as x-api-key. See more info at https://aws.amazon.com/what-is/api-key/"
     if "WWW-Authenticate" in headers:
         parsed_www_auth = www_authenticate.parse(headers['WWW-Authenticate'])
         realm = ''
@@ -75,82 +121,115 @@ def check_auth_type(response):
         # print(parsed_www_auth)
         # print()
 
-        return {"Source":realm,"Name":f"{realm} Auth Endpoint","Description":f"The endpoint is asking for {challenge} Auth. See more info at https://datatracker.ietf.org/doc/html/draft-ietf-http-authentication-03#section-2"}
+        return realm,f"{realm} Auth Endpoint",f"The endpoint is asking for {challenge} Auth. See more info at https://datatracker.ietf.org/doc/html/draft-ietf-http-authentication-03#section-2"
    
 
 
+def main(urls):
 
-weburlURL = sys.argv[1:]
-for url in weburlURL:
-    print()
-    try:
-        """
+    for url in urls:
+        print()
+        try:
+            """
 
-        ## GET ##
-        
-        """
-        r = requests.get(url, verify=True, allow_redirects=False, headers=headers)
-        if r.status_code == 200:
-            output = r.content
-            print(
-                f"{bcolors.OKGREEN}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.OKGREEN}is alive!{bcolors.ENDC}"
-            )
-            if "html" in str(output):
-                print("     Type: HTML")
-            if "xml" in str(output):
-                print("     Type: XML")
-            try:
-                _json = json.loads(str(output))
-                print("     Type: JSON")
-            except:
-                pass
-            print("     size: " + str(convert_size(sys.getsizeof(output))))
-            # print(f"\n{output}")
-        elif r.status_code == 301:
-            redirect_location = r.headers["location"]
-            redirect_location_short = redirect_location.split("?")[0]
-            print(
-                f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
-            )
-        elif r.status_code == 302:
-            redirect_location = r.headers["location"]
-            redirect_location_short = redirect_location.split("?")[0]
-            print(
-                f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
-            )
-        elif r.status_code == 401:
-            auth_type = check_auth_type(r)
-            print(
-                f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Unauthorized{bcolors.ENDC} {bcolors.BOLD}"
-            )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Source']}]{bcolors.ENDC}"
-            )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Name']}]{bcolors.ENDC}"
-            )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Description']}]{bcolors.ENDC}"
-            )
+            ## GET ##
+            
+            """
+            r = requests.get(url, verify=True, allow_redirects=False, headers=headers)
+            if r.status_code == 200:
+                output = r.content
+                print(
+                    f"{bcolors.OKGREEN}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.OKGREEN}is alive!{bcolors.ENDC}"
+                )
+                if "html" in str(output):
+                    print("     Type: HTML")
+                if "xml" in str(output):
+                    print("     Type: XML")
+                try:
+                    _json = json.loads(str(output))
+                    print("     Type: JSON")
+                except:
+                    pass
+                print("     size: " + str(convert_size(sys.getsizeof(output))))
+                # print(f"\n{output}")
+            elif r.status_code == 301:
+                source,name,description = check_auth_on_300(r)
+                redirect_location = r.headers["location"]
+                redirect_location_short = redirect_location.split("?")[0]
+                print(
+                    f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
+                )
+            elif r.status_code == 302:
+                source,name,description = check_auth_on_300(r)
+                redirect_location = r.headers["location"]
+                redirect_location_short = redirect_location.split("?")[0]
+                print(
+                    f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
+                )
+            elif r.status_code == 401:
+                source,name,description = check_auth_type_on_400(r)
+                print(
+                    f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Unauthorized{bcolors.ENDC} {bcolors.BOLD}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
+                )
 
-        elif r.status_code == 403:
-            auth_type = check_auth_type(r)
+            elif r.status_code == 403:
+                source,name,description = check_auth_type_on_400(r)
+                print(
+                    f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Forbidden{bcolors.ENDC} {bcolors.BOLD}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
+                )
+                print(
+                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
+                )
+            else:
+                print(f"Status Code: {r.status_code}")
+        except requests.exceptions.ConnectionError as errc:
             print(
-                f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Forbidden{bcolors.ENDC} {bcolors.BOLD}"
+                f"{bcolors.FAIL}[ERROR]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}refused connection{bcolors.ENDC}"
             )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Source']}]{bcolors.ENDC}"
-            )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Name']}]{bcolors.ENDC}"
-            )
-            print(
-                f"  {bcolors.OKCYAN}[{auth_type['Description']}]{bcolors.ENDC}"
-            )
-        else:
-            print(f"Status Code: {r.status_code}")
-    except requests.exceptions.ConnectionError as errc:
-        print(
-            f"{bcolors.FAIL}[ERROR]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}refused connection{bcolors.ENDC}"
-        )
-        print("     " + str(errc))
+            print("     " + str(errc))
+
+
+
+
+if __name__ == "__main__":
+    args=parser.parse_args()
+
+    raw_output = args.r
+    urls = (args.urls).split(",")
+
+    main(urls)
+
+
