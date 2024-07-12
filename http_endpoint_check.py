7084b2d3de670a60bb0b561e2e6583913920839a
Garrison LeRock <garrison.lerock@kochcc.com>
2024-07-12 16:22:04 -0500

    added recursive redirect functionality

--- http_endpoint_check.py
+++ http_endpoint_check.py
@@ -10,10 +10,16 @@ import urllib.parse
 import re
 
 
-parser=argparse.ArgumentParser(description="HTTP Endpoint Check")
-parser.add_argument('-r', action='store_true', help="return raw output from urls")
-parser.add_argument('-l', action='store_true', default=False, help="follow redirects")
-parser.add_argument('-m','--max-redirects', default='1', type=int, choices=range(1,10), help="max number redirects")
+parser = argparse.ArgumentParser(description="HTTP Endpoint Check")
+parser.add_argument("-r", action="store_true", help="return raw output from urls")
+parser.add_argument("-l", action="store_true", default=False, help="follow redirects")
+parser.add_argument(
+    "-m",
+    default="1",
+    type=int,
+    choices=range(1, 10),
+    help="max number redirects",
+)
 
 parser.add_argument("urls", help="list of urls to check")
 
@@ -56,40 +62,42 @@ headers = {
 # }
 
 
-
-
 def check_auth_on_300(response):
 
-    #document various scenarios, start from the known auth attributes, look for headers and codes
-    #then parse everything else
+    # document various scenarios, start from the known auth attributes, look for headers and codes
+    # then parse everything else
     print(response)
 
     headers = response.headers
     location = urllib.parse.unquote(headers["Location"])
-    #print(location)
+    # print(location)
 
-    #saml keywords for HTTP binding using SAML 2.0
+    # saml keywords for HTTP binding using SAML 2.0
     # according to https://sagarag.medium.com/reloading-saml-saml-basics-b8999995c73e
-    saml_kws = ["sso","saml","samlrequest","relaystate","samlencoding","sigalg"]
+    saml_kws = ["sso", "saml", "samlrequest", "relaystate", "samlencoding", "sigalg"]
     if any(ext in location.lower() for ext in saml_kws):
-        #This is saml redirect HTTP binding
+        # This is saml redirect HTTP binding
         # try to extract this info
         # Regular expressions to extract SAMLRequest, RelayState, and IDP
 
         # For inline saml soo redirect
-        #print(location.lower())
+        # print(location.lower())
         saml_request_pattern = r"SAMLRequest=([^&]+)"
         relay_state_pattern = r"RelayState=([^&]+)"
         idp_pattern = r"=([^&]+)?SAMLRequest"
 
         # Extracting values using regular expressions for saml request
         try:
-            saml_request =  urllib.parse.unquote(re.search(saml_request_pattern, location).group(1))
-            relay_state =  urllib.parse.unquote(re.search(relay_state_pattern, location).group(1))
-            idp =  urllib.parse.unquote(re.search(idp_pattern, location).group(1))
+            saml_request = urllib.parse.unquote(
+                re.search(saml_request_pattern, location).group(1)
+            )
+            relay_state = urllib.parse.unquote(
+                re.search(relay_state_pattern, location).group(1)
+            )
+            idp = urllib.parse.unquote(re.search(idp_pattern, location).group(1))
         except:
-            saml_request =  "none"
-            relay_state =  "none"
+            saml_request = "none"
+            relay_state = "none"
             idp = "none"
         if raw_output:
             print("SAMLRequest:", saml_request)
@@ -102,162 +110,165 @@ def check_auth_on_300(response):
         sp_initiated_kws = ["/idp/sso.saml2"]
         if any(ext in location.lower() for ext in ping_intiated_kws):
             idp = "pingidentity"
-            return f"SAML Auth","SAML SSO",f"Identity Provider initiated {idp}"
+            return f"SAML Auth", "SAML SSO", f"Identity Provider initiated {idp}"
         elif any(ext in location.lower() for ext in sp_initiated_kws):
-            return f"SAML Auth","SAML SSO",f"HTTP SAML Binding found, Service Provider initiated. IdP is {idp}"
+            return (
+                f"SAML Auth",
+                "SAML SSO",
+                f"HTTP SAML Binding found, Service Provider initiated. IdP is {idp}",
+            )
         else:
             idp = "unknown"
-            return f"SAML Auth","SAML SSO",f"Identity Provider {idp}"
+            return f"SAML Auth", "SAML SSO", f"Identity Provider {idp}"
 
-
-
-
-    #print(response.headers)
+    # print(response.headers)
     # look for oidc
     if "Set-Cookie" in headers:
         if "oidc_id_token" in headers["Set-Cookie"]:
-            return "OIDC Auth","OAUTH","OAuth found."
+            return "OIDC Auth", "OAUTH", "OAuth found."
 
 
 def check_auth_type_on_400(response):
 
-    #document various scenarios, start from the known auth attributes, look for headers and codes
-    #then parse everything else
-    #print(response)
+    # document various scenarios, start from the known auth attributes, look for headers and codes
+    # then parse everything else
+    # print(response)
 
     headers = response.headers
 
-    #print(response.headers)
+    # print(response.headers)
 
     if "x-amz-apigw-id" in headers and response.status_code == 403:
-        return "Amazon AWS","API Gateway","You need an API key, likely in header as x-api-key. See more info at https://aws.amazon.com/what-is/api-key/"
+        return (
+            "Amazon AWS",
+            "API Gateway",
+            "You need an API key, likely in header as x-api-key. See more info at https://aws.amazon.com/what-is/api-key/",
+        )
     if "WWW-Authenticate" in headers:
-        parsed_www_auth = www_authenticate.parse(headers['WWW-Authenticate'])
-        realm = ''
-        challenge = ''
-        if 'Basic' in parsed_www_auth:
-            realm = parsed_www_auth['Basic']['realm']
+        parsed_www_auth = www_authenticate.parse(headers["WWW-Authenticate"])
+        realm = ""
+        challenge = ""
+        if "Basic" in parsed_www_auth:
+            realm = parsed_www_auth["Basic"]["realm"]
             challenge = "Basic"
-        if 'Negotiate' in parsed_www_auth:
-            challenge = parsed_www_auth['Negotiate']
-        
+        if "Negotiate" in parsed_www_auth:
+            challenge = parsed_www_auth["Negotiate"]
+
         # print()
         # print(parsed_www_auth)
         # print()
 
-        return realm,f"{realm} Auth Endpoint",f"The endpoint is asking for {challenge} Auth. See more info at https://datatracker.ietf.org/doc/html/draft-ietf-http-authentication-03#section-2"
-   
+        return (
+            realm,
+            f"{realm} Auth Endpoint",
+            f"The endpoint is asking for {challenge} Auth. See more info at https://datatracker.ietf.org/doc/html/draft-ietf-http-authentication-03#section-2",
+        )
+
+
+def test_http_endpoint(url, current_redirect):
+    if current_redirect < 0:
+        return False
+    current_redirect = current_redirect - 1
+    try:
+        r = requests.get(url, verify=True, allow_redirects=False, headers=headers)
+        if r.status_code == 200:
+            output = r.content
+            print(
+                f"{bcolors.OKGREEN}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.OKGREEN}is alive!{bcolors.ENDC}"
+            )
+            if "html" in str(output):
+                print("     Type: HTML")
+            if "xml" in str(output):
+                print("     Type: XML")
+            try:
+                _json = json.loads(str(output))
+                print("     Type: JSON")
+            except:
+                pass
+            print("     size: " + str(convert_size(sys.getsizeof(output))))
+            # print(f"\n{output}")
+            return False
+        elif r.status_code == 301:
+            source, name, description = check_auth_on_300(r)
+            redirect_location = r.headers["location"]
+            redirect_location_short = redirect_location.split("?")[0]
+            print(
+                f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
+            )
+            print(f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}")
+            print(redirect_location_short)
+
+            return test_http_endpoint(redirect_location, current_redirect)
+        elif r.status_code == 302:
+            source, name, description = check_auth_on_300(r)
+            redirect_location = r.headers["location"]
+            redirect_location_short = redirect_location.split("?")[0]
+            print(
+                f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
+            )
+            print(f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}")
+            print(redirect_location_short)
+
+            return test_http_endpoint(redirect_location, current_redirect)
+        elif r.status_code == 401:
+            source, name, description = check_auth_type_on_400(r)
+            print(
+                f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Unauthorized{bcolors.ENDC} {bcolors.BOLD}"
+            )
+            print(f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}")
+            return False
+        elif r.status_code == 403:
+            source, name, description = check_auth_type_on_400(r)
+            print(
+                f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Forbidden{bcolors.ENDC} {bcolors.BOLD}"
+            )
+            print(f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}")
+            print(f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}")
+            return False
+        else:
+            print(f"Status Code: {r.status_code}")
+            return False
+    except requests.exceptions.ConnectionError as errc:
+        print(
+            f"{bcolors.FAIL}[ERROR]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}refused connection{bcolors.ENDC}"
+        )
+        print("     " + str(errc))
+        return False
+    return False
 
 
 def main(urls):
 
     for url in urls:
         print(url)
-        try:
-            """
 
-            ## GET ##
-            
-            """
-            # make this recursive up until the max amount of redirects
-            # follw_redirects will be the boolean to check, otherwise don't do while
-            # maybe make a funciton with a yield? not sure...
-            r = requests.get(url, verify=True, allow_redirects=False, headers=headers)
-            if r.status_code == 200:
-                output = r.content
-                print(
-                    f"{bcolors.OKGREEN}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.OKGREEN}is alive!{bcolors.ENDC}"
-                )
-                if "html" in str(output):
-                    print("     Type: HTML")
-                if "xml" in str(output):
-                    print("     Type: XML")
-                try:
-                    _json = json.loads(str(output))
-                    print("     Type: JSON")
-                except:
-                    pass
-                print("     size: " + str(convert_size(sys.getsizeof(output))))
-                # print(f"\n{output}")
-            elif r.status_code == 301:
-                source,name,description = check_auth_on_300(r)
-                redirect_location = r.headers["location"]
-                redirect_location_short = redirect_location.split("?")[0]
-                print(
-                    f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
-                )
-            elif r.status_code == 302:
-                source,name,description = check_auth_on_300(r)
-                redirect_location = r.headers["location"]
-                redirect_location_short = redirect_location.split("?")[0]
-                print(
-                    f"{bcolors.WARNING}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.WARNING}redirects to{bcolors.ENDC} {bcolors.BOLD}{redirect_location_short}{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
-                )
-            elif r.status_code == 401:
-                source,name,description = check_auth_type_on_400(r)
-                print(
-                    f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Unauthorized{bcolors.ENDC} {bcolors.BOLD}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
-                )
+        """
 
-            elif r.status_code == 403:
-                source,name,description = check_auth_type_on_400(r)
-                print(
-                    f"{bcolors.FAIL}[{r.status_code}]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}Forbidden{bcolors.ENDC} {bcolors.BOLD}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{source}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{name}]{bcolors.ENDC}"
-                )
-                print(
-                    f"  {bcolors.OKCYAN}[{description}]{bcolors.ENDC}"
-                )
-            else:
-                print(f"Status Code: {r.status_code}")
-        except requests.exceptions.ConnectionError as errc:
-            print(
-                f"{bcolors.FAIL}[ERROR]{bcolors.ENDC} {bcolors.BOLD}{url}{bcolors.ENDC} {bcolors.FAIL}refused connection{bcolors.ENDC}"
-            )
-            print("     " + str(errc))
-
-
+        ## GET ##
+        
+        """
+        # make this recursive up until the max amount of redirects
+        # follw_redirects will be the boolean to check, otherwise don't do while
+        # maybe make a funciton with a yield? not sure...
+        requests = []
+        if args.l:
+            test_http_endpoint(url, max_redirects)
+        else:
+            test_http_endpoint(url, 0)
 
 
 if __name__ == "__main__":
-    args=parser.parse_args()
-
+    args = parser.parse_args()
     raw_output = args.r
-    raw_output = args.l
+    follw_redirects = args.l
+    max_redirects = args.m
     urls = (args.urls).split(",")
 
     main(urls)
-
-
